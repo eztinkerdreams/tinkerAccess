9c67370ba4cb9dcf26cedd5a5caac4ab8f7b1dfc
Ronn Erick McQueen <ronnmcqueen@gmail.com>
2016-11-30 22:08:09 -0700

    refactor: refactoring tinker_access_client...

---
+++ tinker_access_client/tests/test_setup.py
@@ -0,0 +1,18 @@
+import unittest
+from mock import patch
+
+import setuptools
+
+from tinker_access_client.tinker_access_client.PackageInfo import PackageInfo
+
+
+class TestSetup(unittest.TestCase):
+
+    @patch.object(setuptools, 'setup')
+    def test_setup(self, mock_setup):
+        PackageInfo.version = 'v1.0-testing'
+        # noinspection PyUnresolvedReferences
+        import tinker_access_client.setup
+        self.assertEqual(mock_setup.call_count, 1)
+        self.assertEqual(mock_setup.call_args[1]['version'], 'v1.0-testing')
+
