560d2a16df71cb6981ec9bdc6d4f07ff7e7371f4
Ronn Erick McQueen <ronnmcqueen@gmail.com>
2016-11-30 22:08:09 -0700

    refactor: refactoring tinker_access_client...

---
+++ tinker_access_client/tests/utils/MockRPi/GPIO/__init__.py
@@ -0,0 +1,18 @@
+from mock import Mock
+
+HIGH = 1
+LOW = 0
+BCM = 11
+IN = 0
+OUT = 1
+PUD_DOWN = 1
+PUD_UP = 2
+setmode = Mock()
+cleanup = Mock()
+setWarnings = Mock()
+setup = Mock()
+# noinspection PyShadowingBuiltins
+input = Mock()
+output = Mock()
+
+
