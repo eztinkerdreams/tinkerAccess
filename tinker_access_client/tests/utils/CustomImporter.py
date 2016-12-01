7ba76e5893d7c7f64448007c160cbf2baeeca2fd
Ronn Erick McQueen <ronnmcqueen@gmail.com>
2016-11-30 22:08:09 -0700

    refactor: refactoring tinker_access_client...

---
+++ tinker_access_client/tests/utils/CustomImporter.py
@@ -0,0 +1,51 @@
+import sys
+from MockLcd import MockLcd
+from MockRPi import MockRPi
+from MockSerial import MockSerial
+
+
+def add_custom_importer():
+    metas = sys.meta_path
+    for meta in metas:
+        if meta.__class__ is CustomImporter:
+            sys.meta_path.remove(meta)
+    sys.meta_path.append(CustomImporter())
+
+
+def is_rpi_module(full_name):
+    return full_name.endswith('.RPi')
+
+
+def is_lcd_module(full_name):
+    return full_name.endswith('.lcdModule')
+
+
+def is_serial_module(full_name):
+    return full_name.endswith('.serial')
+
+
+def is_device_module(full_name):
+    return is_lcd_module(full_name) or is_rpi_module(full_name) or is_serial_module(full_name)
+
+
+class CustomImporter(object):
+
+    def find_module(self, full_name, _):
+        if is_device_module(full_name):
+            return self
+
+        return None
+
+    @staticmethod
+    def load_module(full_name):
+
+        if is_rpi_module(full_name):
+            return MockRPi
+
+        if is_lcd_module(full_name):
+            return MockLcd
+
+        if is_serial_module(full_name):
+            return MockSerial
+
+        raise ImportError(full_name)
