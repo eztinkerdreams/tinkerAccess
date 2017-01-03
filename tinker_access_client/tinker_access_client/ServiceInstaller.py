2a06a214d435b1677b2c51beca00b9acd729a4b9
Ronn Erick McQueen <ronnmcqueen@gmail.com>
2017-01-03 13:01:45 -0700

    enable auto-updates

--- tinker_access_client/tinker_access_client/ServiceInstaller.py
+++ tinker_access_client/tinker_access_client/ServiceInstaller.py
@@ -5,20 +5,27 @@ from ClientLogger import ClientLogger
 
 
 class ServiceInstaller(object):
-
     @staticmethod
     def install(install_lib):
         logger = ClientLogger.setup(phase='install')
         logger.debug('Attempting to install the %s service...', PackageInfo.pip_package_name)
         try:
+            update_script = '{0}{1}/scripts/update.sh'.format(install_lib, PackageInfo.python_package_name)
             install_script = '{0}{1}/scripts/install.sh'.format(install_lib, PackageInfo.python_package_name)
             service_script = '{0}{1}/Service.py'.format(install_lib, PackageInfo.python_package_name)
+
+            # logger.info('update_script: %s', update_script)
+            # logger.info('install_script: %s', install_script)
+            # logger.info('os.getcwd(): %s', os.getcwd())
+            # logger.info('os.path.dirname(os.path.realpath(__file__)): %s', os.path.dirname(os.path.realpath(__file__)))
+            os.chmod(update_script, 0755)
             os.chmod(install_script, 0755)
             os.chmod(service_script, 0755)
 
-            #TODO: refactor to common utility
+            # TODO: refactor to common utility
             cmd = [install_script, '-evx']
-            install_process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            cwd = os.path.dirname(install_script)
+            install_process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd)
             stdout_data, stderr_data = install_process.communicate()
             if install_process.returncode != 0:
                 for ln in stderr_data.splitlines(True):
@@ -32,4 +39,3 @@ class ServiceInstaller(object):
             raise e
 
         logger.debug('the %s service installation succeeded.', PackageInfo.pip_package_name)
-
