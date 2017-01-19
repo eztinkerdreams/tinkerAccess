a15223800ac86983451ef84faaad0433f0d6a6c0
Ronn Erick McQueen <ronnmcqueen@gmail.com>
2017-01-19 03:10:00 -0700

    wip

--- tinker_access_client/tinker_access_client/Client.py
+++ tinker_access_client/tinker_access_client/Client.py
@@ -16,7 +16,7 @@ from Command import Command
 from PackageInfo import PackageInfo
 from ClientLogger import ClientLogger
 from DeviceApi import DeviceApi, Channel
-from CommandHandler import CommandHandler
+from RemoteCommandHandler import RemoteCommandHandler
 from ClientOptionParser import ClientOptionParser, ClientOption
 
 
@@ -102,36 +102,71 @@ class Client(Machine):
                 server.shutdown(socket.SHUT_RDWR)
                 server.close()
 
-    def __handle_status_command(self):
+    def __handle_status_command(self, **kwargs):
         pass
 
-    def __handle_stop_command(self):
+    def __handle_stop_command(self, **kwargs):
         self.__stop()
 
     def __stop(self):
         self.__should_exit = True
         #TODO: wait for client status(self.state) to be done before we exit...
 
+    def __do_something(self):
+        self.__logger.debug('do something...')
+
+    def __trigger_logout(self):
+        self.__logger.debug('trigger logout..')
+
+    def __trigger_training_mode(self):
+        self.__logger.debug('trigger training..')
+
+    def __trigger_login(self):
+        self.__logger.debug('trigger training..')
+
     def __run(self):
-        with CommandHandler() as handler:
+        opts = self.__opts
+
+        with RemoteCommandHandler() as handler, DeviceApi(opts) as device:
             handler.on(Command.STOP, self.__handle_stop_command)
             handler.on(Command.STATUS, self.__handle_status_command)
-            with DeviceApi(self.__opts) as device:
-                #device.on('channdle', 'handler')
-                #etc....
 
-                while not self.__should_exit:
+            device.on(
+                Channel.SERIAL,
+                direction=device.GPIO.IN,
+                call_back=self.__trigger_login
+            )
 
-                    #TODO: remove this message when the complete implementation is final...
-                    self.__logger.debug('%s is running...', PackageInfo.pip_package_name)
+            device.on(
+                Channel.PIN,
+                pin=opts.get(ClientOption.PIN_LOGOUT),
+                direction=device.GPIO.RISING,
+                call_back=self.__trigger_logout
+            )
 
-                    device.wait_for_edge()
+            #TODO: not sure if this will actually work as expected, if we will get two calls, or they will conflict
+            device.on(
+                Channel.PIN,
+                pin=opts.get(ClientOption.PIN_LOGOUT),
+                direction=device.GPIO.RISING,
+                call_back=self.__trigger_training_mode,
+                debounce_delay=2000
+            )
+
+            while not self.__should_exit:
+                self.__logger.debug('%s is waiting...', PackageInfo.pip_package_name)
+                device.wait()
 
     def run(self):
         while not self.__should_exit:
             try:
+                self.__logger.debug('Attempting to start the %s...', PackageInfo.pip_package_name)
                 self.__run()
             except Exception as e:
+                self.__logger.debug('%s failed.', PackageInfo.pip_package_name)
                 self.__logger.exception(e)
+
+            self.__logger.debug('Retrying in 5 seconds...')
+            time.sleep(5)
 
 
