c3616cf7e5587883bf8ccfd01093b381a6dbe004
Ronn Erick McQueen <ronnmcqueen@gmail.com>
2017-01-19 03:10:00 -0700

    wip

--- tinker_access_client/tinker_access_client/Client.py
+++ tinker_access_client/tinker_access_client/Client.py
@@ -1,12 +1,5 @@
-import sys
 import time
-
-import errno
-import socket
-from socket import error as socket_error
-
-# from threading import Thread
-import thread
+import threading
 
 from transitions import Machine
 # TODO: Enable LockedHierarchicalGraphMachine https://github.com/tyarkoni/transitions#-extensions
@@ -16,122 +9,223 @@ from Command import Command
 from PackageInfo import PackageInfo
 from ClientLogger import ClientLogger
 from DeviceApi import DeviceApi, Channel
-from CommandHandler import CommandHandler
+from RemoteCommandHandler import RemoteCommandHandler
 from ClientOptionParser import ClientOptionParser, ClientOption
+
+logout_timer_interval_seconds = 1
 
 
 class State(object):
-    INITIALIZED = 'INITIALIZED'
     IDLE = 'IDLE'
+    IN_USE = 'IN_USE'
+    INITIALIZED = 'INITIALIZED'
+    IN_FAULT = 'IN_FAULT'
+
+
+class Trigger(object):
+    IDLE = 'idle'
+    LOGIN = 'login'
+    LOGOUT = 'logout'
 
 
 class Client(Machine):
     def __init__(self):
+        self.__device = None
+        self.__user_info = None
+        self.__logout_timer = None
+        self.__should_exit = False
         self.__logger = ClientLogger.setup()
         self.__opts = ClientOptionParser().parse_args()[0]
-        self.__should_exit = False
 
         states = []
         for key, _ in vars(State).items():
             if not key.startswith('__'):
                 states.append(key)
 
-        Machine.__init__(self, states=states, initial=State.INITIALIZED)
+        transitions = [
+            {
+                'trigger': Trigger.IDLE,
+                'source': State.INITIALIZED,
+                'dest': State.IDLE
+            },
+            {
+                'trigger': Trigger.LOGIN,
+                'source': State.IDLE,
+                'dest': State.IN_USE,
+                'conditions': 'authorized'
+            },
 
-    #This will be moved to some other class I am sure...
-    def run_listener(self):
-        self.__logger.debug('Attempting to establish %s listener...', PackageInfo.pip_package_name)
+            # TODO: handle case where use re-badges in, or another users badges in while the machine is in use
+            # {
+            #     'trigger': Trigger.LOGIN,
+            #     'source': State.IN_USE,
+            #     'dest': State.IN_USE,
+            #     'conditions': 're-authorized' #extend time if same user, otherwise go through login process
+            # },
 
-        server = None
+            {
+                'trigger': Trigger.LOGIN,
+                'source': State.IDLE,
+                'dest': State.IDLE,
+                'unless': 'authorized',
+                'before': 'not_authorized'
+            },
+            {
+                'trigger': Trigger.LOGOUT,
+                'source': [State.IN_USE, State.IDLE],
+                'dest': State.IDLE
+            }
+        ]
 
-        try:
-            server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        Machine.__init__(self, states=states, transitions=transitions, initial=State.INITIALIZED)
 
-            #TODO: this line causes some major proble, no exception is thrown
-            #listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-            server.bind(('', 8089))
-            server.settimeout(None)
-            server.listen(5)
-
-            now = time.time()
-            while not self.__should_exit:
-                self.__logger.debug('listener started...')
-                client = None
-
-                try:
-                    (client, addr) = server.accept()
-                    command = Command(client.recv(1024))
-                    if command:
-                        self.__logger.debug('%s listener received \'%s\' command',
-                                            PackageInfo.pip_package_name, command)
-
-                        #TODO: allow for graceful shutdown..
-                        if command is Command.STOP:
-                            self.__stop()
-                            break
-                        else:
-                            # send a standard response, possibly JSON...
-                            client.send('State: {0}'.format(self.state))
-
-                    client.shutdown(socket.SHUT_RDWR)
-
-                except socket_error as e:
-                    if e.errno != errno.ENOTCONN:
-                        self.__logger.exception(e)
-                        raise e
-
-                except Exception as e:
-                    self.__logger.exception(e)
-                    raise e
-
-                finally:
-                    if client:
-                        client.close()
-
-                if not self.__should_exit:
-                    self.__logger.debug('will stop listening...')
-
-        except Exception as e:
-            self.__logger.debug('Unable to establish the %s listener.', PackageInfo.pip_package_name)
-            self.__logger.exception(e)
-            thread.interrupt_main()
-
-        finally:
-            if server:
-                server.shutdown(socket.SHUT_RDWR)
-                server.close()
-
-    def __handle_status_command(self):
-        pass
-
-    def __handle_stop_command(self):
+    def __handle_stop_command(self, **kwargs):
         self.__stop()
 
+    # TODO: should only support 'restart' 'stop' doesn't make any sense
+    # as you wouldn't be able to run any addtional commands
     def __stop(self):
+        # what if device is in use, should stop return error, or forcefully do a stop?
+        # TODO: wait for client status(self.state) to be done before we exit...
+        # i.e. logout should be complete etc...
+
+        self.__ensure_idle()
         self.__should_exit = True
-        #TODO: wait for client status(self.state) to be done before we exit...
+        self.__device.stop()
+
+    def __handle_status_command(self, **kwargs):
+        return self.state
+
+    def __enable_power(self):
+        self.__device.write(Channel.PIN, self.__opts.get(ClientOption.PIN_POWER_RELAY), True)
+
+    def __disable_power(self):
+        self.__device.write(Channel.PIN, self.__opts.get(ClientOption.PIN_POWER_RELAY), False)
+
+    def __do_logout(self):
+        self.__cancel_logout_timer()
+
+        if self.__user_info:
+            pass
+            # ServerApi.doLogout
+
+        self.__user_info = None
+
+    def __ensure_idle(self):
+        self.__do_logout()
+        self.__disable_power()
+        self.__device.write(Channel.LED, False, False, True)
+
+    def __ensure_in_use(self):
+        self.__enable_power()
+        self.__device.write(Channel.LED, False, True, False)
+
+    def __logout_timer_tick(self, remaining_seconds):
+        remaining_seconds -= logout_timer_interval_seconds
+
+        if remaining_seconds <= 0:
+            self.logout()
+            return
+
+        if remaining_seconds < 300:
+            self.__device.write(Channel.LED, True, False, False)
+
+        self.__device.write(Channel.LCD, 'Access granted', 'Time Remaining: {0}s'.format(remaining_seconds))
+
+        self.__logout_timer = threading.Timer(
+            logout_timer_interval_seconds,
+            self.__logout_timer_tick,
+            [remaining_seconds, ]
+        )
+        self.__logout_timer.start()
+
+    def __cancel_logout_timer(self):
+        if self.__logout_timer:
+            self.__logout_timer.cancel()
+
+    def __start_logout_timer(self, remaining_seconds):
+        self.__cancel_logout_timer()
+        self.__logout_timer = threading.Timer(
+            logout_timer_interval_seconds,
+            self.__logout_timer_tick,
+            [remaining_seconds, ]
+        )
+        self.__logout_timer.start()
+
+    def authorized(self, *args, **kwargs):
+        if not self.__user_info:
+            badge_code = kwargs.get('badge_code')
+            if badge_code:
+
+                # ServerApi.doLogin
+                if badge_code == 'bar':
+                    self.__user_info = badge_code
+
+        return self.__user_info is not None
+
+    def not_authorized(self, *args, **kwargs):
+        self.__device.write(Channel.LED, True, False, False)
+        self.__device.write(Channel.LCD, 'Access Denied', 'Take the class')
+        time.sleep(2)
+
+    # noinspection PyPep8Naming
+    def on_enter_IN_FAULT(self):
+        self.__device.fault()
+
+    # noinspection PyPep8Naming
+    def on_enter_IN_USE(self, *args, **kwargs):
+        self.__ensure_in_use()
+        self.__device.write(Channel.LCD, 'Access granted')
+
+        # TODO: include user_name, time_remaining, in Access granted for % s granted with time % s"
+        # TODO: start timer for logout trigger
+
+        remaining_seconds = 60  # use value returned from service call, total remaining time should be in seconds
+        self.__start_logout_timer(remaining_seconds)
+
+    # noinspection PyPep8Naming
+    def on_enter_IDLE(self, *args, **kwargs):
+        self.__ensure_idle()
+        self.__device.write(Channel.LCD, 'Scan Badge', 'To Login')
 
     def __run(self):
-        with CommandHandler() as handler:
+        opts = self.__opts
+
+        with RemoteCommandHandler() as handler, DeviceApi(opts) as device:
+            self.__device = device
+
             handler.on(Command.STOP, self.__handle_stop_command)
             handler.on(Command.STATUS, self.__handle_status_command)
-            with DeviceApi(self.__opts) as device:
-                #device.on('channdle', 'handler')
-                #etc....
+            handler.listen()
 
-                while not self.__should_exit:
+            device.on(
+                Channel.SERIAL,
+                direction=device.GPIO.IN,
+                call_back=self.login
+            )
 
-                    #TODO: remove this message when the complete implementation is final...
-                    self.__logger.debug('%s is running...', PackageInfo.pip_package_name)
+            device.on(
+                Channel.PIN,
+                pin=opts.get(ClientOption.PIN_LOGOUT),
+                direction=device.GPIO.RISING,
+                call_back=self.logout
+            )
 
-                    device.wait_for_edge()
+            self.idle()
+            while not self.__should_exit:
+                self.__logger.debug('%s is waiting...', PackageInfo.pip_package_name)
+                device.wait()
 
     def run(self):
         while not self.__should_exit:
             try:
                 self.__run()
             except Exception as e:
+                self.set_state(State.IN_FAULT)
+                self.__logger.debug('%s failed.', PackageInfo.pip_package_name)
                 self.__logger.exception(e)
 
-
+            if not self.__should_exit:
+                self.__logger.debug('Retrying in 5 seconds...')
+                time.sleep(5)
+                self.set_state(State.INITIALIZED)
