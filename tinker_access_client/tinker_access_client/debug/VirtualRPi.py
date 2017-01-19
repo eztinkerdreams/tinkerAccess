9e3244cfe161987de7e9ba947206ab5fa33c1ff9
Ronn Erick McQueen <ronnmcqueen@gmail.com>
2017-01-19 03:10:00 -0700

    wip

--- tinker_access_client/tinker_access_client/debug/VirtualRPi.py
+++ tinker_access_client/tinker_access_client/debug/VirtualRPi.py
@@ -36,6 +36,11 @@ class _VirtualGPIO(object):
     PUD_DOWN = 1
     PUD_UP = 2
 
+    #TODO: confirm these values
+    RISING = 1
+    FALLING = 2
+    BOTH = 3
+
     def __init__(self):
         self.__logger = ClientLogger.setup()
         self.__virtual_device_thread = Thread(target=VirtualDeviceServer.start)
